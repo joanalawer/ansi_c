dc1aaa8b475812cf13cfc00851e7283be8a51a68
joanalawer <lawerjoana477@gmail.com>
2023-01-26 03:55:49 +0000

    Add content to file.

--- c-handbook/breakloop.c
+++ c-handbook/breakloop.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+
+int main(void) {
+
+	int someVariable;
+	for (int i = 0; i <= 10; i++) {
+		if (i == 4 && someVariable == 10) {
+			break;
+		}
+	}
+}
+
+/* break while loop */
+/*	int i = 0;
+	
+	while (1) {
+		/* do something */
+/*		i++;
+		if (i == 10) break;
+	}
+} */

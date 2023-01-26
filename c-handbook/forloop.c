5aa704889dc541add9505a5f1395e90bf5aa9796
joanalawer <lawerjoana477@gmail.com>
2023-01-26 03:57:22 +0000

    Add content to file.

--- c-handbook/forloop.c
+++ c-handbook/forloop.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+
+int main(void) {
+	for (int 1 = 0; i <=10; i++) {
+		printf("%u ", i)
+	}
+}
+
+/* loop from high to low */
+/* int main(void) {
+	for (int 1 = 10; i > 0; i--) {
+		printf("%u ", i)
+	}
+} */
+
+/* loop variable by other values */
+/* int main(void) {
+	for (int 1 = 0; i <1000; i=i+30) {
+		printf("%u ", i)
+	}
+} */

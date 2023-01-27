d2fad80e0ca25925ca02ed2a7b604cb12b4ad506
joanalawer <lawerjoana477@gmail.com>
2023-01-27 03:46:31 +0000

    Program sample on static variables.

---
+++ c-handbook/static.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* In this program 1 is always returne */
+int main(void) {
+    int incrementAge() {
+   /* int age  = 0;
+    age++;
+    return age;
+    }
+}
+
+/* In the program below the value increase by 1 */
+    static int age;
+    age++;
+    return age;
+    }
+    printf("%d\n", incrementAge());
+    printf("%d\n", incrementAge());
+    printf("%d\n", incrementAge());
+}
+
+/*static arrays*/
+    static int ages[3];
+    ages[0]++;
+    return ages[0];
+}

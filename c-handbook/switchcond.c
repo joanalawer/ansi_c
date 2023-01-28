c090312162f1da4153d24c6707ea6280c0a1e9c7
joanalawer <lawerjoana477@gmail.com>
2023-01-28 03:14:55 +0000

    Modified file by adding code.

--- c-handbook/switchcond.c
+++ c-handbook/switchcond.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 
-int main(void) {
-    int  a = 1;
+typedef enum {
+    January, February, March, April, May, June,
+    July, August, September, October, November, December
+} YEARMONTH;
 
-    switch (a) {
+int main(void) {
+    YEARMONTH  month = December;
+
+    switch (month) {
 	case 0:
 	    /*do something*/
+	    printf("We're still in January\n");
 	    break;
+	case 1:
+	    printf("More chocolate in February\n");
+	    break;
+	case 2:
+	    printf("Independence Day in March\n");
+	    break;
+	case 3:
+	    printf("Easter is celebrated in April\n");
+ 	    break;
+	default:
+	    printf("More months to come.\n");
+	    break;
+    }
+}
